a1d650e7636ee180dd570b6a9fa865c2ff7fec5e
tedircks <tedircks@gmail.com>
2020-07-30 18:49:43 -0400

    logic-layer-v1 - token skeleton

---
+++ logic_layer/trivial_purfuit_app/token.py
@@ -0,0 +1,28 @@
+class Token:
+    player_name = None
+    color = None
+    collected_wedges = []
+    location = ()
+    last_location = ()
+    winning_condition = False
+
+    def __init__(self, player_name, color):
+        self.player_name = player_name
+        self.colr = color
+        pass
+
+    def has_category_wedge(self, category):
+        pass
+
+    def has_all_wedges(self):
+        return self.collected_wedges.count == 4
+
+    def set_winning_condition(self, condition):
+        self.winning_condition = condition
+
+    def check_winning_condition(self):
+        return self.winning_condition
+
+    def add_wedge(self, wedge):
+        if (wedge != None):
+            self.collected_wedges.append(wedge)
