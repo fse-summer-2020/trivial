e755e6c143e810d7bc78fb3d9154dc8ba994e52c
Fernando Ortiz <fernando@Fernandos-MacBook-Pro.local>
2020-07-30 22:02:30 -0400

    add get questions, get questions by category, get categories

--- data_layer/trivial_purfuit_data_app/resources/question.py
+++ data_layer/trivial_purfuit_data_app/resources/question.py
@@ -1,7 +1,12 @@
 from flask_restful import Resource
-from trivial_purfuit_data_app.mongo_connector.mongo import get_one_question
-
+from trivial_purfuit_data_app.mongo_connector.mongo import get_questions_by_category, get_all_questions
+from flask_restful import request
 class Question(Resource):
     
     def get(self):
-        return get_one_question()
+        args = request.args
+        if "category_id" in args:
+            category_id = args["category_id"]
+            return get_questions_by_category(category_id)
+        else:
+            return get_all_questions()
