00fa08a5f778cf6507b64a0d321b6d0fd9542c81
tedircks <tedircks@gmail.com>
2020-08-02 09:26:48 -0400

    logic-layer-v1 - add class dictionary method to game state, refactor returns

--- logic_layer/trivial_purfuit_app/__init__.py
+++ logic_layer/trivial_purfuit_app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
+
 def create_app():
     app = Flask(__name__)
     cors = CORS(app, resources={r"*": {"origins": "*"}})
--- logic_layer/trivial_purfuit_app/gamestate.py
+++ logic_layer/trivial_purfuit_app/gamestate.py
@@ -28,7 +28,8 @@ class GameState:
         self.current_state = State.ROLL_DIE
         self.current_player = self.player_order[0]
         self.current_round = 1
-        return self.current_state, self.current_player
+        #return self.current_state, self.current_player
+        return self.get_class_dict()
 
     def set_player_order(self, players):
         for player in players:
@@ -49,39 +50,48 @@ class GameState:
                     if (player.check_winning_condition() == True):
                         if (idx == self.player_order.count - 1):
                             self.current_state = State.GAME_END
-                            return self.current_state, None
+                            #return self.current_state, None
+                            return self.get_class_dict()
             else:
-                return self.current_state, self.current_player
+                #return self.current_state, self.current_player
+                return self.get_class_dict()
         else:
             cur_square_type = self.game_board.get_current_square_type(self.current_player)
             if (cur_square_type == SquareType.CATEGORY): # ----------------------update definition of category square type
                 self.current_state = State.ROLL_DIE
-                return self.current_state, self.current_player
+                #return self.current_state, self.current_player
+                return self.get_class_dict()
+                
             if (cur_square_type == SquareType.HEADQUARTER): # ----------------------update definition of category square type
                  if (self.current_player.has_category_wedge(self.current_trivia_question.category) == False):  #get categetoy from SPACE LOCATION nottttt question
                      self.current_player.add_wedge(self.current_trivia_question.category)   #get categetoy from SPACE LOCATION nottttt question
                      self.current_state = State.ROLL_DIE
-                     return self.current_state, self.current_player
+                     #return self.current_state, self.current_player
+                     return self.get_class_dict()
             else:
                 if (self.current_player.has_all_wedges() == False):
                      self.current_state = State.ROLL_DIE
-                     return self.current_state, self.current_player
+                     #return self.current_state, self.current_player
+                     return self.get_class_dict()
                 else:
                     self.current_player.set_winning_condition(True)
                     if (self.current_round == 1):
                         if (idx == self.player_order.count - 1):
                             self.current_state = State.GAME_END
-                            return self.current_state, None
+                            #return self.current_state, None
+                            return self.get_class_dict()
                         else:
                             self.go_to_next_player()
                             self.current_state = State.ROLL_DIE
-                            return self.current_state, self.current_player
+                            #return self.current_state, self.current_player
+                            return self.get_class_dict()
                     else:
                         self.current_state = State.GAME_END
-                        return self.current_state, None
+                        #return self.current_state, None
+                        return self.get_class_dict()
 
     def get_die_roll(self):
-        diedata = requests.get(url).json()
+        diedata = requests.get(self.url).json()
         dieside = diedata["dice"]
         dieValue = dieside[0].get('value')
         return dieValue
@@ -114,28 +124,51 @@ class GameState:
             if (cur_square_type == SquareType.HEADQUARTER): #PLAYER IS AT INTERSECTION
                 #determine possible moves to gameboard (return square)
                 self.current_state = State.MOVE_DIRECTION
-                return self.current_state, self.current_player
+                #return self.current_state, self.current_player
+                return self.get_class_dict()
             else:
+                #TODO - actually move the player
                 return self.current_state, self.current_player
                 #move player direction to gameboard (return square)
 
         if (cur_square_type == SquareType.ROLL_AGAIN_SQUARE): #player is on roll again square
             self.current_state = State.ROLL_DIE
-            return self.current_state, self.current_player
+            #return self.current_state, self.current_player
+            return self.get_class_dict()
         else:
             if (cur_square_type != SquareType.HEADQUARTER): #player is not on HQ square
                 if(self.current_player.has_all_wedges()): #check if they have all wedges -> to token class
                     self.current_state = State.POLL_CATEGORY_ALL #if they have them all == TRUE poll the players
-                    return self.current_state, self.current_player
+                    #return self.current_state, self.current_player
+                    return self.get_class_dict()
                 else:
                     self.current_state = State.POLL_CATEGORY_CURRENT #let them get a random category?
-                    return self.current_state, self.current_player
+                    #return self.current_state, self.current_player
+                    return self.get_class_dict()
             else:
                 #get categroy suqure from gaembaord
                 #get question category from game factory proxy
                     #this have to calll a random quesiton (make function in question factory to get random question VR specific catgetory)
                 self.question_factory_proxy.get_question(self.game_board.get_current_square(self.current_player).name)
                 self.current_state = State.ANSWER_TRIVIA
-                return self.current_state, self.current_player
+                #return self.current_state, self.current_player
+                return self.get_class_dict()
+
+    def get_class_dict(self):
+        class_dict = self.__dict__
+        items_to_remove = ['player_order', 'factory_proxy', 'game_board', 'url']
+        [class_dict.pop(key) for key in items_to_remove] 
+        return class_dict   
+
+        # player_order = []
+        # current_state = None
+        # current_player = None
+        # current_trivia_question = None
+        # moves_left = None
+        # available_next_squares = None
+        # current_round = None
+        # factory_proxy = None
+        # game_board = None
+        # url = "http://roll.diceapi.com/json/d4"
 
 
