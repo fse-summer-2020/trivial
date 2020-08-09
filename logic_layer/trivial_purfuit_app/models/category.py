1f7305bb877749a693760155cc1e8e121a1c2995
tedircks <tedircks@gmail.com>
2020-08-09 15:57:37 -0400

    logic-layer-v1 - update board layout, add color to category

--- logic_layer/trivial_purfuit_app/models/category.py
+++ logic_layer/trivial_purfuit_app/models/category.py
@@ -2,13 +2,15 @@ import json
 
 class Category(object):
     
-    def __init__(self, name, category_id):
+    def __init__(self, name, category_id, color):
         self.name = name
         self.category_id = category_id
+        self.color = color
     
     def get_dict(self):
         obj = dict()
         obj["name"] = self.name
         obj["category_id"] = self.category_id
+        obj["color"] = self.color
         return obj
         
