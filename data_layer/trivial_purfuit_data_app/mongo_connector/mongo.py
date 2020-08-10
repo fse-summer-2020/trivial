ff07810dd003a6b762ce298a50db5ca427369aac
Fernando Ortiz <fernando@Fernandos-MacBook-Pro.local>
2020-08-10 19:07:59 -0400

    convert ids to json

--- data_layer/trivial_purfuit_data_app/mongo_connector/mongo.py
+++ data_layer/trivial_purfuit_data_app/mongo_connector/mongo.py
@@ -3,7 +3,8 @@ from bson.objectid import ObjectId
 from bson import  json_util
 import json 
 from trivial_purfuit_data_app.mongo_connector.config import config as mongo_config
-client = MongoClient('mongodb+srv://' + mongo_config['username'] + ':' + mongo_config['password'] + '@' + mongo_config['host'])
+client = MongoClient('mongodb://localhost/')
+# client = MongoClient('mongodb+srv://' + mongo_config['username'] + ':' + mongo_config['password'] + '@' + mongo_config['host'])
 db = client['trivial-purfuit-test']
 
 def get_questions_by_category(category_id):
@@ -31,11 +32,12 @@ def save_questions(questions):
             question["_id"] = ObjectId(question["_id"])
         question["category_id"] = ObjectId(question["category_id"])
         ids.append(question_collection.update(spec={"_id":question["_id"]},document=question, upsert=True))
-    return ids
+    return json.loads(json.dumps({"ids":ids}, default=json_util.default))
+
 def save_categories(categories):
     category_collection = db.category
     ids= []
     for category in categories:
         category["_id"] = ObjectId(category["_id"])
         ids.append(category_collection.update(spec={"_id":category["_id"]},document=category, upsert=True))
-    return ids
+    return json.loads(json.dumps({"ids":ids}, default=json_util.default))
